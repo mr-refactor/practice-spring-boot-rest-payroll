dfe68e9912038649713c35695790176823c8cd6f
Tr1ckC0 <trick.co@yahoo.com>
2021-12-18 17:55:24 -0600

    base config

---
+++ src/test/java/com/practice/payroll/entities/EmployeeTest.java
@@ -0,0 +1,11 @@
+package com.practice.payroll.entities;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@SpringBootTest
+public class EmployeeTest {
+}
