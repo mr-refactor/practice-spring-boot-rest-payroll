5f17f8676c329842025e275cbd679d78e44cbe98
Tr1ckC0 <trick.co@yahoo.com>
2021-12-18 18:45:02 -0600

    generated constructors, getters, and setters for Employee

--- src/main/java/com/practice/payroll/entities/Employee.java
+++ src/main/java/com/practice/payroll/entities/Employee.java
@@ -11,6 +11,37 @@ import javax.persistence.Id;
 public class Employee {
 
     private @Id @GeneratedValue Long id;
+    private String name;
+    private String role;
 
     public Employee(){}
+
+    public Employee(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
