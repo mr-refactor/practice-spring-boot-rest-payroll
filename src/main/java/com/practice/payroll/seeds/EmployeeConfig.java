a96e05a94f607f633b5a976f052576d78617b5bb
Tr1ckC0 <trick.co@yahoo.com>
2021-12-18 18:40:30 -0600

    EmployeeSeedTest passed

--- src/main/java/com/practice/payroll/seeds/EmployeeConfig.java
+++ src/main/java/com/practice/payroll/seeds/EmployeeConfig.java
@@ -1,9 +1,23 @@
 package com.practice.payroll.seeds;
 
+import com.practice.payroll.entities.Employee;
+import com.practice.payroll.repositories.EmployeeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class EmployeeConfig {
 
+    private static final Logger log = LoggerFactory.getLogger(EmployeeConfig.class);
 
+    @Bean
+    CommandLineRunner seedDbWithEmployees(EmployeeRepository repository) {
+        return args -> {
+            log.info("Preloading " + repository.save(new Employee()));
+            log.info("Preloading " + repository.save(new Employee()));
+        };
+    }
 }
