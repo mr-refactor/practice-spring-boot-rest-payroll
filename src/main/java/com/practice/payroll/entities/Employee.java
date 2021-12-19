84713c5dd82b473289715a1a8e216833e6ca8761
Tr1ckC0 <trick.co@yahoo.com>
2021-12-18 18:11:07 -0600

    first test passed

--- src/main/java/com/practice/payroll/entities/Employee.java
+++ src/main/java/com/practice/payroll/entities/Employee.java
@@ -1,4 +1,15 @@
 package com.practice.payroll.entities;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+//@Entity
 public class Employee {
+
+//    private @Id @GeneratedValue Long id;
+    public Employee(){}
 }
