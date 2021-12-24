1bb1f8e53b92bae11e1d08bdcf97161bf75b94c9
Tr1ckC0 <trick.co@yahoo.com>
2021-12-24 00:35:00 -0600

    tested employee repo efficiently

--- src/main/java/com/practice/payroll/employee/EmployeeRepository.java
+++ src/main/java/com/practice/payroll/employee/EmployeeRepository.java
@@ -2,6 +2,12 @@ package com.practice.payroll.employee;
 
 import com.practice.payroll.employee.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    public Optional<Employee> findByName(String Name);
 }
--- src/test/java/com/practice/payroll/employee/EmployeeRepositoryTest.java
+++ src/test/java/com/practice/payroll/employee/EmployeeRepositoryTest.java
@@ -1,20 +1,35 @@
 package com.practice.payroll.employee;
 
-import com.practice.payroll.employee.EmployeeRepository;
+import com.fasterxml.jackson.annotation.OptBoolean;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
 public class EmployeeRepositoryTest {
 
     @Autowired
-    private EmployeeRepository repository;
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;
 
     @Test
-    public void repositoryLoads() {
-        assertThat(repository).isNotNull();
+    void findByName_returnsCar() {
+        Employee savedEmployee = entityManager.persistFlushFind(new Employee("Bob Porter", "Supervisor"));
+        Optional<Employee> bob = employeeRepository.findByName("Bob Porter");
+
+        assertThat(bob.isPresent()).isTrue();
+        assertThat(bob.get().getName()).isEqualTo(savedEmployee.getName());
+        assertThat(bob.get().getRole()).isEqualTo(savedEmployee.getRole());
     }
 }
